19d7977685f8b3ad9f8b04a72af0e89508f0f4b0
Willy0921 <91106779+Willy0921@users.noreply.github.com>
2023-05-21 04:55:13 +0800

    add shaking detection and gps files

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import tempfile
 import pvporcupine
 from pvrecorder import PvRecorder
 import speech_recognition as sr
+from shaking_detection import ShakingDetector
 
 
 GUILD = discord.Object(id=1094156105606778940)
@@ -21,12 +22,12 @@ q = queue.Queue()
 
 
 def tts(text, block=True):
-    #print(f'output: {text}')
+    # print(f'output: {text}')
     filename = tempfile.mktemp()
-    gTTS(text, lang='zh-TW').save(filename)
-    command = f'ffplay -autoexit {filename} > /dev/null 2>&1 && rm {filename}'
+    gTTS(text, lang="zh-TW").save(filename)
+    command = f"ffplay -autoexit {filename} > /dev/null 2>&1 && rm {filename}"
     if not block:
-        command += ' &'
+        command += " &"
     os.system(command)
 
 
@@ -44,15 +45,15 @@ class MyClient(discord.Client):
     async def proc_loop(self):
         while not q.empty():
             front = q.get_nowait()
-            if front[0] == 'send_from':
+            if front[0] == "send_from":
                 tts(front[1], False)
-                await front[2].edit(content=f'已傳送「{front[1]}」')
-            elif front[0] == 'send_to':
+                await front[2].edit(content=f"已傳送「{front[1]}」")
+            elif front[0] == "send_to":
                 await self.conv_channel.send(front[1])
-                tts('已傳送訊息', False)
-            elif front[0] == 'get_location':
-                await front[1].edit(content='當前位置為')
-    
+                tts("已傳送訊息", False)
+            elif front[0] == "get_location":
+                await front[1].edit(content="當前位置為")
+
     @proc_loop.before_loop
     async def before_proc_loop(self):
         await self.wait_until_ready()
@@ -66,57 +67,57 @@ client = MyClient(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user} (ID: {client.user.id})')
-    print('------')
+    print(f"Logged in as {client.user} (ID: {client.user.id})")
+    print("------")
 
 
 @client.tree.command()
-@app_commands.describe(text='要傳送的訊息')
+@app_commands.describe(text="要傳送的訊息")
 async def send(interaction: discord.Interaction, text: str):
-    await interaction.response.send_message(f'傳送「{text}」中...')
+    await interaction.response.send_message(f"傳送「{text}」中...")
     msg = await interaction.original_response()
-    q.put(['send_from', text, msg])
+    q.put(["send_from", text, msg])
 
 
 @client.tree.command()
 async def get_location(interaction: discord.Interaction):
-    await interaction.response.send_message('請稍等...')
+    await interaction.response.send_message("請稍等...")
     msg = await interaction.original_response()
-    q.put(['get_location', msg])
+    q.put(["get_location", msg])
 
 
 def stt():
-    '''
+    """
     i, _, _ = select.select([sys.stdin], [], [], timeout=10)
     if i:
         return sys.stdin.readline().strip()
     else:
         return None
-    '''
+    """
     r = sr.Recognizer()
     text = None
     with sr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
         try:
-            text = r.recognize_google(audio, language='zh-TW')
-            print(f'Google Speech Recognition thinks you said {text}')
+            text = r.recognize_google(audio, language="zh-TW")
+            print(f"Google Speech Recognition thinks you said {text}")
         except sr.UnknownValueError:
-            print('Google Speech Recognition could not understand audio')
+            print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
-            print(f'Could not request results from Google Speech Recognition service; {e}')
+            print(
+                f"Could not request results from Google Speech Recognition service; {e}"
+            )
     return text
 
+
 porcupine = pvporcupine.create(
-  access_key='O+W0Bvushxv3+glLzhqPbSxmh5yYVbmxqCKG8gcFNxtyMdI+N+bOjQ==',
-  keyword_paths=['keyword/pi.ppn'],
-  model_path='keyword/porcupine_params_zh.pv'
+    access_key="O+W0Bvushxv3+glLzhqPbSxmh5yYVbmxqCKG8gcFNxtyMdI+N+bOjQ==",
+    keyword_paths=["keyword/pi.ppn"],
+    model_path="keyword/porcupine_params_zh.pv",
 )
 
-recorder = PvRecorder(
-    device_index=-1,
-    frame_length=porcupine.frame_length
-)
+recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
 recorder.start()
 
 
@@ -127,21 +128,28 @@ def recv_loop():
 
         if result >= 0:
             recorder.stop()
-            tts('請說指令')
+            tts("請說指令")
             str = stt()
             if str is None:
-                tts('未接收到指令')
-            elif '傳送訊息' in str:
-                tts('請說訊息')
+                tts("未接收到指令")
+            elif "傳送訊息" in str:
+                tts("請說訊息")
                 str = stt()
                 if str is not None:
-                    q.put_nowait(['send_to', str])
+                    q.put_nowait(["send_to", str])
                 else:
-                    tts('未接收到訊息')
+                    tts("未接收到訊息")
             else:
-                tts('未知指令')
+                tts("未知指令")
             recorder.start()
 
 
+def shaking_detect_loop():
+    sd = ShakingDetector()
+    while True:
+        sd.detect(q)
+
+
 threading.Thread(target=recv_loop, daemon=True).start()
-client.run('MTA5NDEwMjUwNjQ0NDk0NzQ3Ng.GlTrjK.sZlNWR-yfgWmWUZKbqA0iaidKrWN7gt0eFbOzI')
+threading.Thread(target=shaking_detect_loop, daemon=True).start()
+client.run("MTA5NDEwMjUwNjQ0NDk0NzQ3Ng.GlTrjK.sZlNWR-yfgWmWUZKbqA0iaidKrWN7gt0eFbOzI")
---
+++ shaking_detection.py
@@ -0,0 +1,51 @@
+import time
+import smbus
+import numpy as np
+
+from imusensor.MPU9250 import MPU9250
+
+
+class ShakingDetector:
+    def __init__(self):
+        self.address = 0x68
+        self.bus = smbus.SMBus(1)
+        self.imu = MPU9250.MPU9250(self.bus, self.address)
+        self.imu.begin()
+        self.imu.loadCalibDataFromFile("./calibration/calib.json")
+        self.prev_accel = np.array(self.imu.AccelVals)
+        self.irregular_times = 0
+
+    def detect(self, q):
+        """
+        1. Read the acceleration data
+        2. Compute the difference between the current acceleration and the previous acceleration
+        3. Compute the norm of the difference
+        4. If the norm is greater than a threshold, then we consider it as a collision
+        """
+        self.imu.readSensor()
+        self.imu.computeOrientation()
+
+        cur_accel = np.array(self.imu.AccelVals)
+
+        accel_diff = cur_accel - self.prev_accel
+        delta_accel = np.sqrt(np.sum(np.power(accel_diff, 2)))
+
+        # print(f"Accel x: {imu.AccelVals[0]} ; Accel y : {imu.AccelVals[1]} ; Accel z : {imu.AccelVals[2]}")
+        if delta_accel > 20:
+            self.irregular_times += 1
+            print(f"delta acceleration: {delta_accel}")
+        else:
+            self.irregular_times = 0
+
+        if self.irregular_times >= 2:
+            q.put_nowait(["send_to", "偵測到異常震動"])
+
+            # print(f"roll: {imu.roll} ; pitch : {imu.pitch} ; yaw : {imu.yaw}")
+        time.sleep(0.1)
+        self.prev_accel = cur_accel
+
+
+if __name__ == "__main__":
+    detector = ShakingDetector()
+    while True:
+        detector.detect()
